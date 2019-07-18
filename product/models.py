2bc790d8d4e4bde1b5bb7b3f1fd2c94a818304f3
Jethet <henriette.hettinga@gmail.com>
2019-07-18 16:08:01 +0200

    Create product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+rom django.db import models
 
-# Create your models here.
+# generally speaking, you will have one model per app
+# Django documentation has info on model field types to choose from
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateField()
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=400)
