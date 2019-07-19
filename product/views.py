da046ba7eaaa5782433a17db201899df70302196
Jethet <henriette.hettinga@gmail.com>
2019-07-19 12:37:45 +0200

    Change views and urls for home page

--- product/views.py
+++ product/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-from .models import Product
 
 def home(request):
-    product = Product.objects
-    return render(request, 'product/home.html', {'product':product})
+    return render(request, 'product/home.html')
--- producthunt/urls.py
+++ producthunt/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-import product.views
+from product.views import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', product.views.home, name='home'),
+    path('', views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
