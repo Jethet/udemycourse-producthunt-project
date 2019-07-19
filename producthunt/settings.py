dc3c6f9cf6734e96c36f1b00e1b91771ee1f957b
Jethet <henriette.hettinga@gmail.com>
2019-07-19 12:41:49 +0200

    Add accounts to installed apps

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'product.apps.ProductConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
