bcf06a4fef8d983034197f29070ffbd4a62668f3
Jethet <henriette.hettinga@gmail.com>
2019-07-21 13:12:57 +0200

    Create logout method

--- accounts/views.py
+++ accounts/views.py
@@ -30,4 +30,7 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def logout(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+    
