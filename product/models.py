ff04a826409421b73d93047c936d7ab55949c689
Jethet <henriette.hettinga@gmail.com>
2019-07-21 15:08:32 +0200

    Add date method and summary method

--- product/models.py
+++ product/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
 
 # generally speaking, you will have one model per app
 # Django documentation has info on model field types to choose from
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    product_url = models.URLField([source])
+    product_url = models.URLField()
     pub_date = models.DateTimeField(default=datetime.date.today)
-    votes_total = models.IntegerField([source])
+    votes_total = models.IntegerField(default=1)
     image = models.ImageField(upload_to='images/')
     icon = models.ImageField(upload_to='images/', height_field=0, width_field=0)
     body = models.CharField(max_length=400)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:200]
 
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
