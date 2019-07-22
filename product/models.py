8ffe8601d263abfe1fc125cadf73fefb3f6d43ea
Jethet <henriette.hettinga@gmail.com>
2019-07-22 17:30:22 +0200

    Adjust model for body text and size of url

--- product/models.py
+++ product/models.py
@@ -7,12 +7,13 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    url = models.TextField()
+    body = models.CharField()
+    url = models.TextField(max_length=900)
     pub_date = models.DateTimeField()
     votes_total = models.IntegerField(default=1)
     image = models.ImageField(upload_to='images/')
     icon = models.ImageField(upload_to='images/')
-    body = models.CharField(max_length=400)
+
     hunter = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
