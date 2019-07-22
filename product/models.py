43c2bd6a7baf27df2a984e1287065d8a4038bf6e
Jethet <henriette.hettinga@gmail.com>
2019-07-22 10:21:53 +0200

    Change field in model

--- product/models.py
+++ product/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    url = models.URLField()
+    url = models.TextField()
     pub_date = models.DateTimeField(default=datetime.date.today)
     votes_total = models.IntegerField(default=1)
     image = models.ImageField(upload_to='images/')
